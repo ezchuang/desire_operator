d42c2c3da9db028522988602006a3b2d99095631
ezchuang <129792088+ezchuang@users.noreply.github.com>
2023-12-28 22:41:36 +0800

    fix: fix the alt description of image

--- src/frontendFunctions/types/NullSign.tsx
+++ src/frontendFunctions/types/NullSign.tsx
@@ -4,8 +4,8 @@ const NullSign: React.FC = () => (
   <img
     className="h-5 min-h-[16px] aspect-auto"
     src="https://d2ndnbzl6dhb4z.cloudfront.net/null2.svg"
-    alt="描述性文字"
-    onError={(event) => (event.currentTarget.src = "預設圖片的URL")}
+    alt="空值"
+    onError={(event) => (event.currentTarget.src = "空值圖片的URL")}
   />
 );
 
